14d9ee95ad2cb83d20075b909e162bd7245fb17c
Олег Луцик <oleglutsik.dev@gmail.com>
2024-06-24 11:54:12 +0300

    Добавьте файлы проекта.

---
+++ DigitSumSorter/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] array = { 121, 75, 81 };
+
+        Console.WriteLine("Original array:");
+        Console.WriteLine(string.Join(", ", array));
+
+        var sortedArrayDescending = array
+            .OrderByDescending(num => num.ToString().Sum(c => c - '0'))
+            .ToArray();
+
+        Console.WriteLine("Sorted array in descending order by sum of digits:");
+        Console.WriteLine(string.Join(", ", sortedArrayDescending));
+
+        var sortedArrayAscending = array
+            .OrderBy(num => num.ToString().Sum(c => c - '0'))
+            .ToArray();
+
+        Console.WriteLine("Sorted array in ascending order by sum of digits:");
+        Console.WriteLine(string.Join(", ", sortedArrayAscending));
+    }
+}
